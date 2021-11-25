4a83cbf4e15eb43513d0c852ec0d55b5b017414f
JigSaW <misho_1998@mail.ru>
2021-11-25 17:25:33 +0300

    fib c++

---
+++ gcd.py
@@ -0,0 +1,40 @@
+'''
+По данным двум числам 1 <= a, b <= 2 * 10^9 
+найдите их наибольший общий делитель.
+
+Sample Input 1:
+18 35
+
+Sample Output 1:
+1
+
+Sample Input 2:
+14159572 63967072
+
+Sample Output 2:
+4
+'''
+
+def gcd(a, b):
+    if a == 0:
+        return b 
+    elif b == 0:
+        return a 
+    elif a >= b:
+        return gcd(a % b, b)
+    elif a <= b:
+        return gcd(a, b % a)
+
+
+def main():
+    a, b = map(int, input().split())
+    print(gcd(a, b))
+
+
+if __name__ == "__main__":
+    main()
+
+'''
+def gcd(a, b):
+    return gcd(b, a % b) if b else a
+'''
